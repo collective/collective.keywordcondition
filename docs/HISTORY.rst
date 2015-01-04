49c16dbd57c30e5326fea0cd7adfec46ac81faa5
Wouter Vanden Hove <wouter.vanden.hove@gmail.com>
2015-01-04 13:41:14 +0100

    add autoinclude-entrypoint

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,9 +1,29 @@
-Changelog for collective.keywordcondition
+Changelog
+=========
 
-    (name of developer listed in brackets)
 
-collective.keywordcondition - 1.0b1 Unreleased
+0.1 (unreleased)
+----------------
 
-    - Initial package structure.
-      [zopeskel]
+- Add ``MANIFEST.in``.
+  [WouterVH]
 
+- Added ``z3c.autoinclude``-entrypoint to mark this as a Plone-plugin.
+  This avoids the need to explicitly load the zcml-slug.
+  [WouterVH]
+
+- Migrate package form Collective-SVN to Github.
+  Cfr. https://github.com/collective/collective.keywordcondition
+  [WouterVH]
+
+- Not zip-safe. 
+  [nouri]
+
+1.0b1 (2007-12-20)
+------------------
+
+- Initial implementation.
+  [optilude]
+
+- Initial package structure.
+  [zopeskel]
--- setup.py
+++ setup.py
@@ -1,34 +1,49 @@
+# -*- coding: utf-8 -*-
+import os
 from setuptools import setup, find_packages
 
-version = '1.0b2'
 
-setup(name='collective.keywordcondition',
-      version=version,
-      description="A condition for Plone 3's content rules that checks keywords on a content item",
-      long_description="""\
-""",
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.1.dev0'
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('docs/HISTORY.rst')
+    + '\n' +
+    read('CONTRIBUTORS.rst')
+    )
+
+setup(
+    name='collective.keywordcondition',
+    version=version,
+    description="A condition for Plone 3's content rules that checks keywords on a content item",
+    long_description=long_description,
+
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Framework :: Zope2",
         "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='Plone content rules',
-      author='Martin Aspeli',
-      author_email='optilude@gmail.com',
-      url='http://plone.org',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    keywords='Plone content rules',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='https://github.com/collective/collective.keywordcondition',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        ],
+
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = plone',
+        },
+    )
